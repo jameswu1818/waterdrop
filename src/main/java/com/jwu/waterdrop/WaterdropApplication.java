4291efcb327b8b0ef949298111d2b1a23b3e96e3
James Wu <jameswu800@gmail.com>
2017-06-21 07:35:52 -0400

    Skeleton spring build with mV

---
+++ src/main/java/com/jwu/waterdrop/WaterdropApplication.java
@@ -0,0 +1,12 @@
+package com.jwu.waterdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterdropApplication.class, args);
+	}
+}
